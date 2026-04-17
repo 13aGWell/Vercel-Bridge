ada2fbb2e1438fde6c1e77b7401b00fa845790f2
13aGWell <94050851+13aGWell@users.noreply.github.com>
2026-04-17 15:37:45 +0330

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+const http = require('http');
+
+export default function handler(req, res) {
+    const options = {
+        hostname: '5.75.193.168', // آی‌پی سرور آلمان
+        port: 80,
+        path: '/darbasti',
+        method: req.method,
+        headers: {
+            ...req.headers,
+            host: '5.75.193.168' // تنظیم هدر هاست برای جلوگیری از بلاک شدن
+        }
+    };
+
+    const proxy = http.request(options, (targetRes) => {
+        res.writeHead(targetRes.statusCode, targetRes.headers);
+        targetRes.pipe(res);
+    });
+
+    req.pipe(proxy);
+
+    proxy.on('error', (e) => {
+        res.status(500).send("Proxy Error: " + e.message);
+    });
+}
