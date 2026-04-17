8a6475b8e592fe4539f1cf3d2794dce2759aa01b
13aGWell <94050851+13aGWell@users.noreply.github.com>
2026-04-17 15:44:30 +0330

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const http = require('http');
 
 export default function handler(req, res) {
     const options = {
-        hostname: '5.75.193.168', // آی‌پی سرور آلمان
+        hostname: '5.75.193.168',
         port: 80,
         path: '/darbasti',
         method: req.method,
         headers: {
-            ...req.headers,
-            host: '5.75.193.168' // تنظیم هدر هاست برای جلوگیری از بلاک شدن
+            'Host': '5.75.193.168', // اجباری برای شناسایی در مقصد
+            'Connection': 'upgrade', // برای حمایت از وب‌سوکت
+            'Upgrade': 'websocket'
         }
     };
 
@@ -20,6 +21,7 @@ export default function handler(req, res) {
     req.pipe(proxy);
 
     proxy.on('error', (e) => {
-        res.status(500).send("Proxy Error: " + e.message);
+        console.error("Vercel Proxy Error:", e.message);
+        res.status(502).json({ error: "Gateway Error", details: e.message });
     });
 }
